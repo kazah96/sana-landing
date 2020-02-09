833c86a0de95465c99dacbcae3fbfc0f65ec4f60
Артём Таранюк <poquepoque27@gmail.com>
2020-02-09 19:58:31 +0500

    Updates

---
+++ precache-manifest.379adeea6c9cf9e3c57fa67b687f2cbd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1175128e96d29ec08cad9aa5b51b4222",
+    "url": "/sana-landing/index.html"
+  },
+  {
+    "revision": "1893527fd8f053032450",
+    "url": "/sana-landing/static/css/main.c64bd143.chunk.css"
+  },
+  {
+    "revision": "254c4f1ce9df7d8e7635",
+    "url": "/sana-landing/static/js/2.d3bed9a9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/sana-landing/static/js/2.d3bed9a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1893527fd8f053032450",
+    "url": "/sana-landing/static/js/main.418dfa48.chunk.js"
+  },
+  {
+    "revision": "3db9d09a291c1596ae48",
+    "url": "/sana-landing/static/js/runtime-main.b2f72746.js"
+  },
+  {
+    "revision": "bcc8f1fd8bc7a9de5f3ad931fa7d7fd5",
+    "url": "/sana-landing/static/media/bigbon.bcc8f1fd.jpg"
+  },
+  {
+    "revision": "864dc597d9cec95a1d48a208c12f03e4",
+    "url": "/sana-landing/static/media/escape.864dc597.png"
+  },
+  {
+    "revision": "e57f208eb18dbc8037608d7ee2bddf3e",
+    "url": "/sana-landing/static/media/miwa.e57f208e.png"
+  }
+]);
