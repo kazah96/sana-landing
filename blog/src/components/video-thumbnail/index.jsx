f5f2c6623e73026bc5befcf2886cb5aac18a84bd
Артём Таранюк <poquepoque27@gmail.com>
2020-02-11 08:45:10 +0500

    Variative plitka

--- blog/src/components/video-thumbnail/index.jsx
+++ blog/src/components/video-thumbnail/index.jsx
@@ -6,11 +6,16 @@ class Header extends PureComponent {
   static propTypes = {
     imgUrl: PropTypes.string,
     name: PropTypes.string,
+    width: PropTypes.number,
+  }
+
+  static defaultProps = {
+    width: 1,
   }
 
   render() {
     return (
-      <span className="thumbnail">
+      <span className="thumbnail" style={{ width: `${this.props.width}px` }}>
         <img src={this.props.imgUrl} alt="" className="img" />
         <div className="label">{this.props.name}</div>
       </span>
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,26 +4,79 @@ import VideoThumb from "../components/video-thumbnail"
 import { Link, graphql } from "gatsby"
 
 import "./style.css"
+import Media from "react-media"
+// import Settings from "../components/misc-settings"
+
 import Layout from "../components/layout"
 
+const margin = 4;
+
+const settings = {
+  max_plitok_mobile: 1,
+  plitka_width_mobile: 450,
+  plitka_width_medium: 350,
+  plitka_width_large: 550,
+}
+
+
 class Portfolio extends PureComponent {
+  state = {
+    ...settings
+  }
+
+  onSettingsChange = values => {
+    this.setState({ ...values })
+  }
+
   render() {
     const videos = this.props.data.allStrapiVideo.edges.sort((a, b) => {
-      if (a.node.order > b.node.order) return 1
-      if (a.node.order < b.node.order) return -1
+      if (a.node.orderby > b.node.orderby) return 1
+      if (a.node.orderby < b.node.orderby) return -1
       return 0
     })
 
     return (
       <Layout>
+        {/* <Settings
+          settings={settings}
+          onChange={this.onSettingsChange}
+        /> */}
         <div className="thumbnails-container">
-          {videos.map(({ node: item }) => {
-            return (
-              <Link to={`/videos/${item.title}`}>
-                <VideoThumb imgUrl={item.image_url} name={item.title} />
-              </Link>
-            )
-          })}
+          <Media
+            queries={{
+              small: "(max-width: 740px)",
+              medium: "(min-width: 741px)",
+              large: "(min-width: 1101px)",
+            }}
+          >
+            {matches =>
+              videos.map(({ node: item }, index) => {
+                let width = item.width || 1;
+
+                if (matches.small) width = this.state.max_plitok_mobile
+                if (matches.medium)
+                  width = width === 1 ? 1 : index % 2 === 0 ? width - 1 : 1
+
+                let w = this.state.plitka_width_mobile
+                if (matches.medium) w = this.state.plitka_width_medium
+                if (matches.large) w = this.state.plitka_width_large;
+
+                const qq =
+                  parseInt(width) * w +
+                  margin * (width - 1)
+
+                return (
+                  <Link to={`/videos/${item.title}`}>
+                    <VideoThumb
+                      imgUrl={item.image_url}
+                      name={item.title}
+                      width={qq}
+                    />
+                  </Link>
+                )
+              })
+            }
+          </Media>
         </div>
       </Layout>
     )
@@ -41,7 +94,8 @@ export const pageQuery = graphql`
           title
           video_url
           image_url
-          order
+          orderby
+          width
         }
       }
     }
