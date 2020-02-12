a82038584f3214d5f37be0fa78988beccb14d183
Артём Таранюк <poquepoque27@gmail.com>
2020-02-12 20:08:18 +0500

    Доавил fluent-ffmpeg, можно выбирать библиотеку в конфиге

--- blog/convert-settings.js
+++ blog/convert-settings.js
@@ -1,16 +1,19 @@
 const path = require(`path`)
 
 module.exports = {
-  outWidth: '12',
-  outHeight: '120',
-  keepPixelAspectRatio: true,
+  // библиотека которая непосредственно кодирует
+  // Так же можно попробовать вариант "fluent-ffmpeg"
+  library: 'fluent-ffmpeg',
+  outWidth: '350',
+  outHeight: '200',
+  keepPixelAspectRatio: false,
   keepAspectRatio: false,
-  bitrate: 100,  // ПОКА НЕ РАБОТАЕТ
-  frameRate: 2,
-  aspectRatio: 1.77,
+  bitrate: '2k', // больше - качественнее, но больше размер (не работает в ffmpeg, только в fluent-ffmpeg)
+  frameRate: 1,
+  aspectRatio: 5,
   outputFormat: 'webm', // в принципе менять не надо
   inputFormat: 'mp4', // это тоже наверно не стоит менять
   codec: 'mpeg4', // ПОКА НЕ РАБОТАЕТ
   outFolder: './video-thumbs/output',
-  inputFolder: './videos'
+  inputFolder: './videos',
 }
